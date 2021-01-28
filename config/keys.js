38a5f13a165dfcc0dfdc6b5a2fa20c2814f7df81
Sristi Chowdhury <53037053+Sristi27@users.noreply.github.com>
2021-01-28 20:40:12 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports=
 {
 	secret:process.env.JWT_SEC || "eertyu",
-	MONGOURL: process.env.MONGODB_URI  ||  "mongodb+srv://sristi:9SydKHQzKmfFSQJl@cluster0.9f1qo.mongodb.net/<dbname>?retryWrites=true&w=majority"
+	MONGOURL: process.env.MONGODB_URI  ||  "mongodb+srv://sristi:<password>@cluster0.9f1qo.mongodb.net/<dbname>?retryWrites=true&w=majority"
 }
